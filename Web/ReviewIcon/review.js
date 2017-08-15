a2d829d9fc3a71702791ebaa6550c0467a8bc15f
sakiw0124 <sakiw0124@gmail.com>
2017-08-15 18:40:14 +0800

    20170815_select的選項大困擾，website清單下有style清單，碰到怎麽呈現、怎麽設預設值、怎麽取得的問題

--- Web/ReviewIcon/review.js
+++ Web/ReviewIcon/review.js
@@ -1,19 +1,22 @@
 var app = angular.module("app1",[]);
 
 app.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
-    $scope.items = [];
-    $http.get('filelist/customicondesign-office1-reflection.txt').success(function(data){
-    angular.forEach(data,function(item, ind){
-      $scope.items.push(item);
-    });
-  });
-}]);
-
-app.controller('PathCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.paths = [];
     $http.get('pathlist/website.txt').success(function(data){
       angular.forEach(data,function(item, ind){
         $scope.paths.push(item);
       });
+      $scope.sel = {style : $scope.paths[0].style};
     });
+}]);
+
+app.controller('PathCtrl', ['$scope', '$http', function($scope, $http) {
+    $scope.items = [];
+    $scope.website = "customicondesign.com";
+    $scope.style = "customicondesign-office1-reflection";
+    $http.get('filelist/' + $scope.website + '/' + $scope.style + '.txt').success(function(data){
+    angular.forEach(data,function(item, ind){
+      $scope.items.push(item);
+    });
+  });
 }]);
