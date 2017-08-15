e8a0ccb50fa58a6dbf35479827d044ea2a2c523d
sakiw0124 <sakiw0124@gmail.com>
2017-08-15 15:59:14 +0800

    20170814_想要用網頁來呈現像ACDsee看圖的樣貌

---
+++ Web/ReviewIcon/review.js
@@ -0,0 +1,36 @@
+function MainCtrl($scope) {
+    
+ 
+  $scope.items = [
+    
+    {header: 'add1.png', 
+     path: '../Icon/customicondesign.com/customicondesign-office1-reflection/png/add1.png'},
+    
+    {header: 'add1-16.png', 
+     path: 'Z:\Picture\test\Icon\customicondesign.com\customicondesign-office1-reflection\png\add1-16.png'},
+    
+    {header: 'add1-24.png', 
+     path: 'Z:\Picture\test\Icon\customicondesign.com\customicondesign-office1-reflection\png\add1-24.png'},
+    
+    {header: 'add1-32.png',
+     path:'Z:\Picture\test\Icon\customicondesign.com\customicondesign-office1-reflection\png\add1-32.png'},
+    
+    {header: 'add1-48.png',
+     path:'Z:\Picture\test\Icon\customicondesign.com\customicondesign-office1-reflection\png\add1-48.png'},
+     
+    {header: 'add1-64.png',
+     path:'Z:\Picture\test\Icon\customicondesign.com\customicondesign-office1-reflection\png\add1-64.png'},
+     
+    {header: 'add1-128.png',
+     path:'Z:\Picture\test\Icon\customicondesign.com\customicondesign-office1-reflection\png\add1-128.png'},
+     
+    {header: 'add2.png',
+     path:'Z:\Picture\test\Icon\customicondesign.com\customicondesign-office1-reflection\png\add2.png'},
+     
+    {header: 'add2-16.png',
+     path:'Z:\Picture\test\Icon\customicondesign.com\customicondesign-office1-reflection\png\add2-16.png'},
+     
+    {header: 'add2-32.png',
+     path:'Z:\Picture\test\Icon\customicondesign.com\customicondesign-office1-reflection\png\add2-32.png'},
+  ];
+}
