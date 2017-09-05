87c10f0bd18d9845b181568ee6b014f657e6ecf0
sakiw0124 <sakiw0124@gmail.com>
2017-09-05 17:21:52 +0800

    20170905_直接製作完robamroute.bat後就執行。 此外，將robamcmd.bat的捷徑按右鍵內容->進階，設定以系統管理員執行，就可以單雙擊執行一次做完。(記住一定要是捷徑才可以這麼設定)

--- Windows_Batch/robamcmd.bat
+++ Windows_Batch/robamcmd.bat
@@ -5,4 +5,6 @@ for /F "usebackq tokens=1-5" %%i IN (`route print^|find "10.0.2."`) DO set gatew
 echo route delete 10.0.0.0 > "D:\Saki\Documents\My Work\19. 微服務\老闆電器\99. 開發環境\robamroute.bat"
 echo route add 172.18.0.0 mask 255.255.0.0 %gateway% if %interface% metric 59 >> "D:\Saki\Documents\My Work\19. 微服務\老闆電器\99. 開發環境\robamroute.bat"
 echo pause >> "D:\Saki\Documents\My Work\19. 微服務\老闆電器\99. 開發環境\robamroute.bat"
+
+call "D:\Saki\Documents\My Work\19. 微服務\老闆電器\99. 開發環境\robamroute.bat"
 pause
--- Windows_Batch/robamroute.bat
+++ Windows_Batch/robamroute.bat
@@ -1,3 +1,3 @@
 route delete 10.0.0.0 
-route add 172.18.0.0 mask 255.255.0.0 10.0.2.3 if 38 metric 59 
+route add 172.18.0.0 mask 255.255.0.0 10.0.2.66 if 39 metric 59 
 pause 
